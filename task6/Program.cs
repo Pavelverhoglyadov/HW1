de40e6a3afde3431cf93004e777c88f3338dba37
pavel <dongod@bk.ru>
2022-11-26 19:35:39 +0400

    сделал task6

---
+++ task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число для проверки на четность");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 % 2 == 0)
+Console.WriteLine("число четное");
+else
+Console.WriteLine("число нечетное");
+
+    
+
