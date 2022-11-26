9fd8c8ff6e529fcd39c69c6f90b51cfaa5dcd32c
pavel <dongod@bk.ru>
2022-11-26 21:17:41 +0400

    сделал task8

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+for (int ind =1;ind <= num1; ind++)
+
+if (ind % 2 == 0)
+{
+  Console.WriteLine(ind);  
+}
+
+
