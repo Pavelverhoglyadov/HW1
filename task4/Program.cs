97b7b1d9bead09bd4d6a044bc25592e75e38b495
pavel <dongod@bk.ru>
2022-11-26 19:12:41 +0400

    доделал tas4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,10 +1,11 @@
-﻿Console.WriteLine("первое число");
+﻿Console.WriteLine("введите первое число");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число");
+Console.WriteLine("введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число");
+Console.WriteLine("введите третие число");
 int num3 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
 if (num2 > max)max = num2;
 if (num3 > max)max = num3;
+Console.Write("это max число ");
 Console.WriteLine(max);
