03c72c6039869ab9fa6770bab970d09cef8ed07e
pavel <dongod@bk.ru>
2022-11-26 15:59:49 +0400

    сделал task2

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+Console.WriteLine(num1);
+else
+Console.WriteLine(num2);
